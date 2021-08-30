12f711b453b1888f6a17c81fd58c76287c331a45
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-08-30 15:01:11 -0300

    Added base list

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      storedResources: [
+        {id:'vue-doc', 
+        title: 'Vue Documentation', 
+        description: 'Site with official Vue.js documentation',
+        link: 'https://vuejs.org/v2/guide/'},
+        {id:'bootstrap-vue-doc', 
+        title: 'Bootstrap-Vue Documentation', 
+        description: 'Site with official Bootstrap Vue documentation',
+        link: 'https://bootstrap-vue.org/'},
+        {id:'vuetify-doc', 
+        title: 'Vuetify Documentation', 
+        description: 'Site with official Vuetify documentation',
+        link: 'https://vuetifyjs.com/en/getting-started/installation/'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
