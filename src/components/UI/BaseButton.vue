32f1fe4e10872e2d13992b91ffa84465f2081fdc
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-09-02 14:19:17 -0300

    Added button style

--- src/components/UI/BaseButton.vue
+++ src/components/UI/BaseButton.vue
@@ -11,5 +11,30 @@ export default {
 </script>
 
 <style scoped>
+  button {
+    padding: 0.75rem 1.5rem;
+    font-family: inherit;
+    background-color: #ce0071;
+    border: 1px solid #ce0071;
+    border-radius: 8px;
+    color: white;
+    cursor: pointer;
+  }
 
+  button:hover,
+  button:active {
+    background-color: #d14190;
+    border-color: #d14190;
+  }
+
+  .flat {
+    background-color: transparent;
+    color: #ce0071;
+    border: none;
+  }
+
+  .flat:hover,
+  .flat:active {
+    background-color: #e065a9;
+  }
 </style>
